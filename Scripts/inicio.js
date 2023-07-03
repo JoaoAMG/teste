61c7cf4ed2170e093f63e7521004041b559a83d3
JoaoAMG <138420757+JoaoAMG@users.noreply.github.com>
2023-07-03 00:18:28 -0300

    commit init
    projeto teste1

---
+++ Scripts/inicio.js
@@ -0,0 +1,6 @@
+function cadastrar() {
+   var dados = { nome:""}
+    dados.nome=document.getElementById("Nome").value;
+    document.getElementById("nomec").innerHTML="obrigado por contribuir"+nome;
+};
+
